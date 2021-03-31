4d6b45a50f54b98d7d16701c6eb8bbf47e6d10bb
djam1012 <80195758+djam1012@users.noreply.github.com>
2021-03-31 20:51:09 +0200

    Update Chaine.h

--- Chaine.h
+++ Chaine.h
@@ -38,7 +38,7 @@ int compter_points_chaine(CellChaine* liste_chaine);
 Chaines* lectureChaines(FILE *f);
 void ecrireChaines(Chaines *C, FILE *f);
 void afficheChainesSVG(Chaines *C, char* nomInstance);
-double longueurChaine (CellChaine * c );
+double longueurChaine (CellChaine* c);
 double longueurTotale(Chaines *C);
 int comptePointsTotal(Chaines *C);
 
