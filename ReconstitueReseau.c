6d33461e058bc54136b46447183e00af058cf577
djam1012 <80195758+djam1012@users.noreply.github.com>
2021-04-05 17:21:02 +0200

    Update ReconstitueReseau.c

--- ReconstitueReseau.c
+++ ReconstitueReseau.c
@@ -5,9 +5,32 @@
 #include "Reseau.h"
 #include "SVGwriter.h"
 
+void methode_liste(Chaines* C){
+  Reseau* R = reconstitueReseauListe(C);
+  afficher_reseau(R);
+  afficher_voisins_reseau(R);
+
+  printf("\n");
+  printf("NOMBRE DE COMMODITES DU RESEAU : %d\n", nbCommodites(R));
+  printf("NOMBRE DE LIAISONS DU RESEAU : %d\n", nbLiaisons(R));
+
+  ecrireReseau(R, NULL);
+  afficheReseauSVG(R, "reseau_affiche");
+}
+
+void methode_table(Chaines* C){
+
+  return;
+}
+
+void methode_arbre(Chaines* C){
+
+  return;
+}
+
 int main(int argc, char const *argv[]) {
   if (argc!=2){
-    printf("Il faut un seul fichier d'entrée et un seul entier > 0.\n");
+    printf("Il faut un seul fichier d'entrée.\n");
     return 1;
   }
 
@@ -18,6 +41,7 @@ int main(int argc, char const *argv[]) {
     printf("Problème lors de la lecture du fichier\n");
     return 1;
   }
+
   printf("fichier ouvert avec succès\n\n");
   Chaines** pointeur_chaines=(Chaines**)malloc(sizeof(Chaines*));
   *pointeur_chaines = lectureChaines(fic);
@@ -34,9 +58,31 @@ int main(int argc, char const *argv[]) {
 
   ecrireChaines(*pointeur_chaines, fic2); // le fichier sera fermé dans le fonction
 
-  Reseau* R = reconstitueReseauListe(*pointeur_chaines);
-  afficher_reseau(R);
+  //afficher_liste_chaines(*pointeur_chaines);
 
+  printf("Choisissez la méthode de stockage de votre réseau, 1 2 ou 3:\n");
+  printf("-1 : par liste chaînée\n");
+  printf("-2 : par table de hachage\n");
+  printf("-3 : par arbre\n");
+  int val;
+  int lire=scanf("%d", &val);
+
+  switch(val){
+    case 1:
+      methode_liste(*pointeur_chaines);
+      break;
+    case 2:
+      methode_table(*pointeur_chaines);
+      break;
+    case 3:
+      methode_arbre(*pointeur_chaines);
+      break;
+    default:
+      printf("Votre choix est invalide\n");
+      break;
+  }
+
+  printf("-------------------------------------------------\n");
   liberer_liste_chaines(*pointeur_chaines);
   free(pointeur_chaines);
   fclose(fic);
