5bd638649f541e0e6d92afb938ed946c1e91236e
djam1012 <80195758+djam1012@users.noreply.github.com>
2021-04-11 16:09:23 +0200

    Update ReconstitueReseau.c

--- ReconstitueReseau.c
+++ ReconstitueReseau.c
@@ -2,11 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Chaine.h"
+#include "Reseau.h"
 #include "SVGwriter.h"
+
+void methode_liste(Chaines* C){
+  Reseau* R = reconstitueReseauListe(C);
+  afficher_reseau(R);
+
+  printf("\n");
+  printf("\033[1;33m");
+  printf("NOMBRE DE COMMODITES DU RESEAU : %d\n", nbCommodites(R));
+  printf("NOMBRE DE LIAISONS DU RESEAU : %d\n", nbLiaisons(R));
+  printf("\033[0;37m");
+
+
+  ecrireReseau(R, NULL);
+  afficheReseauSVG(R, "reseau_affiche");
+  liberer_reseau(R);
+}
+
+void methode_table(Chaines* C){
+
+  return;
+}
+
+void methode_arbre(Chaines* C){
+
+  return;
+}
 
 int main(int argc, char const *argv[]) {
   if (argc!=2){
-    printf("Il faut un seul fichier d'entrée et un seul entier > 0.\n");
+    printf("Il faut un seul fichier d'entrée.\n");
     return 1;
   }
 
@@ -17,6 +44,7 @@ int main(int argc, char const *argv[]) {
     printf("Problème lors de la lecture du fichier\n");
     return 1;
   }
+
   printf("fichier ouvert avec succès\n\n");
   Chaines** pointeur_chaines=(Chaines**)malloc(sizeof(Chaines*));
   *pointeur_chaines = lectureChaines(fic);
@@ -32,15 +60,34 @@ int main(int argc, char const *argv[]) {
   printf("fichier ouvert avec succès\n\n");
 
   ecrireChaines(*pointeur_chaines, fic2); // le fichier sera fermé dans le fonction
-  afficher_liste_chaines(*pointeur_chaines);
 
-  afficheChainesSVG(*pointeur_chaines, "chaines1");
-  printf("\033[1;36m");
-  printf("LONGUEUR TOTALE DE LA CHAINE:   %f\n", longueurTotale(*pointeur_chaines));
-  printf("NOMBRE TOTAL DE POINTS:         %d\n", comptePointsTotal(*pointeur_chaines));
+  //afficher_liste_chaines(*pointeur_chaines);
+
+  printf("\033[1;39m");
+  printf("Choisissez la méthode de stockage de votre réseau, 1 2 ou 3:\n");
+  printf("-1 : par liste chaînée\n");
+  printf("-2 : par table de hachage\n");
+  printf("-3 : par arbre\n");
   printf("\033[0;37m");
+  int val;
+  int lire=scanf("%d", &val);
 
+  switch(val){
+    case 1:
+      methode_liste(*pointeur_chaines);
+      break;
+    case 2:
+      methode_table(*pointeur_chaines);
+      break;
+    case 3:
+      methode_arbre(*pointeur_chaines);
+      break;
+    default:
+      printf("Votre choix est invalide\n");
+      break;
+  }
 
+  printf("-------------------------------------------------\n");
   liberer_liste_chaines(*pointeur_chaines);
   free(pointeur_chaines);
   fclose(fic);
